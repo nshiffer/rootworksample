57a2283489bb91c88cdfbd8a6f104f024aa33095
Nick Shiffer <38705476+nshiffer@users.noreply.github.com>
2020-11-25 23:29:22 -0500

    Added print function for printing the expected output

--- create_driver.py
+++ create_driver.py
@@ -1,4 +1,18 @@
 class Driver:
   def __init__(self, name):
     self.name = name
-    self.speed = 0
+    self.avgSpeed = 0
+    self.totalDist = 0
+    self.pastTrips = []
+  
+  def prettyPrintInfo(self):
+    if self.totalDist > 1:
+      distString = self.totalDist+" miles"
+    elif self.totalDist == 1:
+      distString = self.totalDist+" mile"
+    else:
+      #Distance is zero so only print distance
+      print(self.name+": "+self.totalDist+" miles")
+      return
+    
+    print(self.name+": "+distStringt+" @ "+self.avgSpeed+" mph")
